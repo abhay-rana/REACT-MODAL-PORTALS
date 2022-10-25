bebeb0a6a9bec7b4eb223118b9b1184e5b4e3b6a
abhay-rana <abhayrana286@gmail.com>
2022-10-25 17:32:09 +0530

    input field

--- src/components/utils/form/input.js
+++ src/components/utils/form/input.js
@@ -1,12 +1,32 @@
 import React, { forwardRef, memo, useEffect, useRef } from "react";
+import { ReactComponent as Error } from "../../../assets/svg/cross.svg";
 
-const Input = ({ label, name, value, topHeight = 40, floatingLabel = true, className, placeholder, required = false, ...restProps }, ref) => {
+const Input = (
+	{
+		label,
+		name,
+		value,
+		topHeight = 40,
+		floatingLabel = true,
+		onErrorClick = () => {},
+		className,
+		placeholder,
+		required = false,
+		error,
+		iconImg,
+		...restProps
+	},
+	ref
+) => {
 	const input_ref = useRef();
 	let margin_top = "";
+	let extra_class = "";
 	let floating_className = "uppercase absolute left-0  ";
 	if (!floatingLabel) {
 		floating_className = `uppercase `;
 	}
+
+	if (!!error) extra_class = `border-1 border-red-600 `;
 
 	const getRef = (el) => {
 		input_ref.current = el;
@@ -24,7 +44,7 @@ const Input = ({ label, name, value, topHeight = 40, floatingLabel = true, class
 	return (
 		<>
 			<div
-				className={`relative ${margin_top}`}
+				className={`relative inline-block ${margin_top} h-full w-full border-2 border-black ${className}`}
 				style={{ marginTop: `${topHeight}px` }}
 			>
 				{/* absolute will be respective to the parent and loose its old position */}
@@ -34,17 +54,35 @@ const Input = ({ label, name, value, topHeight = 40, floatingLabel = true, class
 					name={name}
 					onFocus={addPlaceholder}
 					onBlur={removePlaceholder}
-					className="peer rounded-[5px] border-2 border-black p-[10px] outline-none transition-colors duration-150 focus:border-violet-600"
+					className={`peer w-full rounded-[5px] border-2 border-black p-[10px] outline-none transition-colors duration-150 focus:border-violet-600 ${extra_class}`}
 					{...restProps}
 				/>
 				{label ? (
 					<label
 						htmlFor={name}
-						className={`${floating_className} transition:all ease rounded-md p-[10px] duration-300 peer-focus:translate-x-2 peer-focus:-translate-y-2 peer-focus:bg-white peer-focus:py-[1px] peer-focus:text-sm peer-focus:text-violet-700`}
+						className={`${floating_className} transition:all ease rounded-md p-[10px] duration-300 peer-focus:translate-x-2 peer-focus:-translate-y-3 peer-focus:bg-white peer-focus:px-3 peer-focus:py-0 peer-focus:text-sm peer-focus:text-violet-700`}
 					>
 						{label}
+						{required ? <span className="absolute text-red-600">*</span> : null}
 					</label>
 				) : null}
+				{error ? (
+					<>
+						<p className="text-red-600">{error}</p>
+						<div
+							className="absolute right-0 top-1 cursor-pointer p-[10px] hover:scale-125"
+							onClick={onErrorClick}
+						>
+							{!iconImg ? (
+								<Error
+									fill="red"
+									className="h-4 w-4"
+								/>
+							) : null}
+						</div>
+					</>
+				) : null}
+				{!!iconImg ? <div className="absolute right-0 top-1 cursor-pointer  p-[3px] hover:scale-125">{iconImg}</div> : null}
 			</div>
 		</>
 	);
--- src/custom-input.js
+++ src/custom-input.js
@@ -2,49 +2,79 @@ import React, { useState } from "react";
 
 import Input from "./components/utils/form/input";
 
+import { ReactComponent as Download } from "./assets/svg/download.svg";
+
 const CustomInput = () => {
 	const [state, setState] = useState({});
+	const [error, setError] = useState("");
 
 	const onChangeInput = (key, value) => {
 		setState({ ...state, [key]: value });
 	};
 
+	const setErr = () => {
+		if (error) setError("");
+		else setError("This is Error");
+	};
+
+	const fireError = () => {
+		console.log("hello abhay");
+	};
 	return (
 		<>
-			<Input
-				label="Username"
-				name="username"
-				value={state.password}
-				onChange={(e) => onChangeInput(e.target.name, e.target.value)}
-				type="input"
-				placeholder={"Enter Username"}
-				autoComplete="off"
-				floatingLabel
-				topHeight={30}
-			/>
-			<Input
-				topHeight={50}
-				label="Password"
-				name="password"
-				value={state.password}
-				onChange={(e) => onChangeInput(e.target.name, e.target.value)}
-				type="input"
-				placeholder={"Enter password"}
-				autoComplete="off"
-				floatingLabel
-			/>
-			<Input
-				name="name"
-				label="Name"
-				value={state.password}
-				onChange={(e) => onChangeInput(e.target.name, e.target.value)}
-				type="input"
-				placeholder={"Enter "}
-				autoComplete="off"
-				required
-				floatingLabel
-				className="mt-6"
-			/>
+			<div className="flex flex-col">
+				<Input
+					label="Username"
+					name="username"
+					value={state.password}
+					onChange={(e) => onChangeInput(e.target.name, e.target.value)}
+					type="input"
+					placeholder={"Enter Username"}
+					autoComplete="off"
+					floatingLabel
+					topHeight={30}
+				/>
+				<div className="m-4 h-[50px] w-[300px]">
+					<Input
+						topHeight={50}
+						label="Password"
+						name="password"
+						value={state.password}
+						onChange={(e) => onChangeInput(e.target.name, e.target.value)}
+						type="input"
+						placeholder={"Enter password"}
+						autoComplete="off"
+						floatingLabel
+						required
+						onFocus={setErr}
+						error={error}
+						onErrorClick={fireError}
+					/>
+				</div>
+				<div className="h-[100px] w-[250px]">
+					<Input
+						name="name"
+						label="Name"
+						value={state.password}
+						onChange={(e) => onChangeInput(e.target.name, e.target.value)}
+						type="input"
+						placeholder={"Enter "}
+						autoComplete="off"
+						required
+						disabled
+						error="This is error message"
+						floatingLabel
+						className="mt-6"
+						iconImg={
+							<Download
+								className="h-6 w-6"
+								fill="red"
+							/>
+						}
+					/>
+				</div>
+			</div>
+			<button onClick={setErr}>Set Error</button>
 		</>
 	);
 };
